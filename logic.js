9c37055d4e4641a692ef1fb69f00311a11325bdb
dakota stuart <dakotastuart@dakotas-MacBook-Pro.local>
2019-05-19 13:26:28 -0700

    added style to buttons.

--- logic.js
+++ logic.js
@@ -2,27 +2,26 @@ var topics = ["Skateboarding", "Music", "Video Games", "Programming", "Archery"]
 
 function renderButtons() {
 
-    $("#buttons-view").empty();
+    $('#buttons-view').empty();
 
     // Looping through the array of topics
     for (var i = 0; i < topics.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
+      // Generating buttons for each movie in the array
+      var a = $('<button>');
       // Adding a class of topic to our button
-      a.addClass("topic");
+      a.addClass('topic btn btn-outline-info');
       // Adding a data-attribute
-      a.attr("data-name", topics[i]);
+      a.attr('data-name', topics[i]);
       // Providing the initial button text
       a.text(topics[i]);
       // Adding the button to the buttons-view div
-      $("#buttons-view").append(a);
+      $('#buttons-view').append(a);
 
       a.on('click', function (){
-        var topic= $(this).attr("data-name");
+        var topic= $(this).attr('data-name');
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=87fUMW1SGqF7lZH0BdAHZEJC4CnG0sHq&limit=10";
+        topic + "&api_key=87fUMW1SGqF7lZH0BdAHZEJC4CnG0sHq&limit=10&rating=g";
     
         $.ajax({
           url: queryURL,
@@ -32,31 +31,31 @@ function renderButtons() {
             var results = response.data;
     
             for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div>");
+              var gifDiv = $('<div>');
     
               var rating = results[i].rating;
     
-              var p = $("<p>").text("Rating: " + rating);
+              var p = $('<p>').text("Rating: " + rating);
     
-              var topicImage = $("<img>");
+              var topicImage = $('<img>');
               topicImage.attr("src", results[i].images.fixed_height.url);
     
               gifDiv.prepend(p);
               gifDiv.prepend(topicImage);
     
-              $("#topic-view").prepend(gifDiv);
+              $('#topic-view').prepend(gifDiv);
             }
         });
           
       });
     }
 }
-// This function handles events where one button is clicked
-$("#add-topic").on("click", function(event) {
+//Function events where one button is clicked
+$('#add-topic').on('click', function(event) {
     event.preventDefault();
 
-    // This line grabs the input from the textbox
-    var topic = $("#topic-input").val().trim();
+    // Grabbing the input from the textbox
+    var topic = $('#topic-input').val().trim();
     if (topic){
         topics.push(topic);
     }
